e4e668281834c0bf6cd30552dfe36975edc3f38e
gilhan <rlfgks97@gmail.com>
2022-03-05 19:52:54 +0900

    chore:

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 function onLoginSumbit(event) {
+    // 브랑줘의 기본 동작을 막는 함수
     event.preventDefault();
     // const username = loginInput.value;
     // console.log(username);
