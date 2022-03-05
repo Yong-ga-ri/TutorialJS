c3fe2ae68e48645a872d78503353776f1a65b0da
gilhan <rlfgks97@gmail.com>
2022-03-05 21:44:56 +0900

    #4.6

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+// 중요하지않은 string 변수는 대문자로만 표기한다
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSumbit(event) {
-    // 브랑줘의 기본 동작을 막는 함수
+    // 브라우저의 기본 동작을 막는 함수
+    // JS는 기본 동작을 막을 수 있음
     event.preventDefault();
-    // const username = loginInput.value;
-    // console.log(username);
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    // greeting.innerText = "Hello " + username;
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSumbit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSumbit);
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);
+}
