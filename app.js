17b034db761f1f1ac4fcab6b45a9a5ec30b88689
gilhan <rlfgks97@gmail.com>
2022-03-05 19:17:20 +0900

    #4-2

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-
-    console.dir(loginInput.value);
-    console.log("click!!!");
+    const username = loginInput.value;
+    console.log(username)
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
