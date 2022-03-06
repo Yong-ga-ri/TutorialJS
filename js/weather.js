be9cb6e45532fd1b0367c8e7a2e82dee3f3f718f
gilhan <rlfgks97@gmail.com>
2022-03-06 16:42:40 +0900

    #8-2~3

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,18 @@
+const API_KEY = "need to Sign in"
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat`
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+
     console.log("You live in", lat, lng);
 }
 
